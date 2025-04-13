faa46ba4cf1aac64378c91ec9d50595894f425d1
IbrahimMurad <ibrahimmorad31@gmail.com>
2025-04-13 15:34:07 +0200

    adding breadcrumbs to scope views and templates

--- scope/views.py
+++ scope/views.py
@@ -5,7 +5,13 @@ from scope.models import TextBook, Unit, Chapter, Lesson
 def index(request):
     textbooks = TextBook.objects.all()
     return render(
-        request, "scope/index.html", {"title": "Textbooks", "scopes": textbooks}
+        request,
+        "scope/index.html",
+        context={
+            "title": "Textbooks",
+            "scopes": textbooks,
+            "breadcrumbs": [],
+        },
     )
 
 
@@ -15,25 +21,43 @@ def units(request, textbook_id):
     return render(
         request,
         "scope/index.html",
-        {"title": textbook.title, "scopes": units},
+        context={
+            "title": textbook.title,
+            "scopes": units,
+            "breadcrumbs": [textbook],
+        },
     )
 
 
 def chapters(request, unit_id):
     unit = get_object_or_404(Unit, id=unit_id)
-    chapters = Chapter.objects.filter(unit=unit)
+    chapters = Chapter.objects.filter(unit=unit).prefetch_related("unit__textbook")
     return render(
         request,
         "scope/index.html",
-        {"title": unit.title, "scopes": chapters},
+        context={
+            "title": unit.title,
+            "scopes": chapters,
+            "breadcrumbs": [unit.textbook, unit],
+        },
     )
 
 
 def lessons(request, chapter_id):
     chapter = get_object_or_404(Chapter, id=chapter_id)
-    lessons = Lesson.objects.filter(chapter=chapter)
+    lessons = Lesson.objects.filter(chapter=chapter).prefetch_related(
+        "chapter__unit__textbook"
+    )
     return render(
         request,
         "scope/index.html",
-        {"title": chapter.title, "scopes": lessons},
+        context={
+            "title": chapter.title,
+            "scopes": lessons,
+            "breadcrumbs": [
+                chapter.unit.textbook,
+                chapter.unit,
+                chapter,
+            ],
+        },
     )
