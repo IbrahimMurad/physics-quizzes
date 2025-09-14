5c961c016ab6a22e6b326edbfb85a19b55962504
ibrahimmurad <ibrahimmorad31@gmail.com>
2025-09-14 17:57:34 +0300

    Create custom scope admin to add filters, search, and actions for admin site.

--- scope/admin.py
+++ scope/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import Scope
 
-admin.site.register(Scope)
+
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ("title", "level", "is_published")
+    list_filter = ("level", "is_published", "parent")
+    search_fields = ["title"]
+
+    @admin.action(description="Publish selected scopes")
+    def publish(self, request, queryset):
+        queryset.update(is_published=True)
+
+    @admin.action(description="Unpublish selected scopes")
+    def unpublish(self, request, queryset):
+        queryset.update(is_published=False)
+
+    actions = [publish, unpublish]
+
+
+admin.site.register(Scope, ScopeAdmin)
